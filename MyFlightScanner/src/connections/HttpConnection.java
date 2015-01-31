a8feb97743b366ff03705e68e7dbd47a32806697
dedema87 <manzi.daniele@gmail.com>
2015-01-31 14:44:51 +0100

    upgrade

--- MyFlightScanner/src/connections/HttpConnection.java
+++ MyFlightScanner/src/connections/HttpConnection.java
@@ -1,5 +1,63 @@
 package connections;
 
-public class HttpConnection {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
 
+public class HttpConnection {
+	
+	private Proxy proxy;
+	private BufferedReader buffReader;
+	
+	
+	public HttpConnection(String url) throws Exception {
+		super();
+		initialize(url);
+	}
+	
+	
+	public HttpConnection(String ipProxyAddr, int proxyPort, String url) throws Exception {
+		super();
+		setProxy(ipProxyAddr, proxyPort);
+		initialize(url);
+	}
+
+	
+	public void setProxy(String ipAddr, int port) {
+		this.proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ipAddr, port));
+	}
+	
+	
+	public BufferedReader getBufferedReader() {
+		return this.buffReader;
+	}
+	
+	
+	public void initialize(String url) throws Exception {
+		
+		URL urlConn;
+		HttpURLConnection connection = null;
+		
+		try {
+			urlConn = new URL(url);
+			connection = (HttpURLConnection) ((proxy != null) ? urlConn.openConnection(proxy) : urlConn.openConnection());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			throw new MalformedURLException("The connection cannot be established (Malformed URL):\n" + e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new Exception("The connection cannot be established (I/O Exception):\n" + e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("The connection cannot be established (Generic Exception):\n" + e.getMessage());
+		}
+		
+		buffReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	}
+	
 }
--- MyFlightScanner/src/utility/Utils.java
+++ MyFlightScanner/src/utility/Utils.java
@@ -1,5 +1,14 @@
 package utility;
 
+import java.io.BufferedReader;
+
+import connections.HttpConnection;
+
 public class Utils {
+	
+	public static BufferedReader automaticCreationHttpConnection(String url) throws Exception {
+		HttpConnection httpConn = new HttpConnection(url);
+		return httpConn.getBufferedReader();
+	}
 
 }
