95fdc0915a9f3dbbbd8f8a84a1a83301ab6c27fc
dedema87 <manzi.daniele@gmail.com>
2015-01-31 16:16:19 +0100

    fix

--- MyFlightScanner/src/requestresponse/VolaGratisRequest.java
+++ MyFlightScanner/src/requestresponse/VolaGratisRequest.java
@@ -33,6 +33,20 @@ public static String urlDepartureAndArrivalAR(String departureAirportCode, Strin
 		ret.append("&adults=");
 		ret.append(people);
 		ret.append("&infants=0&SPM=-1&classFlight=&roundtrip=true&search1_airports_hidden=&search1_focusctrl=&search1_keywords=&search2_airports_hidden=&search2_focuctrl=&search2_keywords=&filter1=TXT&filter1=TXT&bf_subsource=S02HPF10S02RR11&routeFilter=&url=search3.do");
+		ret.append("&outboundDay=");
+		if (from.get(Calendar.DAY_OF_MONTH) < 10)	ret.append(0);
+		ret.append(from.get(Calendar.DAY_OF_MONTH));
+		ret.append("&outboundMonthYear=");
+		if (from.get(Calendar.MONTH)+1 < 10) ret.append(0);
+		ret.append(from.get(Calendar.MONTH)+1);
+		ret.append(from.get(Calendar.YEAR));
+		ret.append("&returnDay=");
+		if (to.get(Calendar.DAY_OF_MONTH) < 10)	ret.append(0);
+		ret.append(to.get(Calendar.DAY_OF_MONTH));
+		ret.append("&returnMonthYear=");
+		if (to.get(Calendar.MONTH)+1 < 10) ret.append(0);
+		ret.append(to.get(Calendar.MONTH)+1);
+		ret.append(to.get(Calendar.YEAR));
 		
 		return ret.toString();
 	}
--- MyFlightScanner/src/utility/Utils.java
+++ MyFlightScanner/src/utility/Utils.java
@@ -1,6 +1,7 @@
 package utility;
 
 import java.io.BufferedReader;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,26 +13,26 @@ public class Utils {
 	public static Map<Integer, String> monthMap = new HashMap<Integer, String>();
 	
 	static {
-		daysOfWeekMap.put(1, "Lun");
-		daysOfWeekMap.put(2, "Mar");
-		daysOfWeekMap.put(3, "Mer");
-		daysOfWeekMap.put(4, "Gio");
-		daysOfWeekMap.put(5, "Ven");
-		daysOfWeekMap.put(6, "Sab");
-		daysOfWeekMap.put(7, "Dom");
+		daysOfWeekMap.put(Calendar.MONDAY, "Lun");
+		daysOfWeekMap.put(Calendar.TUESDAY, "Mar");
+		daysOfWeekMap.put(Calendar.WEDNESDAY, "Mer");
+		daysOfWeekMap.put(Calendar.THURSDAY, "Gio");
+		daysOfWeekMap.put(Calendar.FRIDAY, "Ven");
+		daysOfWeekMap.put(Calendar.SATURDAY, "Sab");
+		daysOfWeekMap.put(Calendar.SUNDAY, "Dom");
 		
-		monthMap.put(0, "Gennaio");
-		monthMap.put(1, "Febbraio");
-		monthMap.put(2, "Marzo");
-		monthMap.put(3, "Aprile");
-		monthMap.put(4, "Maggio");
-		monthMap.put(5, "Giugno");
-		monthMap.put(6, "Luglio");
-		monthMap.put(7, "Agosto");
-		monthMap.put(8, "Settembre");
-		monthMap.put(9, "Ottobre");
-		monthMap.put(10, "Novembre");
-		monthMap.put(11, "Dicembre");
+		monthMap.put(Calendar.JANUARY, "Gennaio");
+		monthMap.put(Calendar.FEBRUARY, "Febbraio");
+		monthMap.put(Calendar.MARCH, "Marzo");
+		monthMap.put(Calendar.APRIL, "Aprile");
+		monthMap.put(Calendar.MAY, "Maggio");
+		monthMap.put(Calendar.JUNE, "Giugno");
+		monthMap.put(Calendar.JULY, "Luglio");
+		monthMap.put(Calendar.AUGUST, "Agosto");
+		monthMap.put(Calendar.SEPTEMBER, "Settembre");
+		monthMap.put(Calendar.OCTOBER, "Ottobre");
+		monthMap.put(Calendar.NOVEMBER, "Novembre");
+		monthMap.put(Calendar.DECEMBER, "Dicembre");
 	}
 	
 	public static BufferedReader automaticCreationHttpConnection(String url) throws Exception {
